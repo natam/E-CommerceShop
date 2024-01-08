45ea7a8a037a1be0bbbd06ac66b9e8ade880f7c5
natalliakharchenko <morozko.nnn@gmail.com>
2024-01-08 18:16:24 +0100

    SpringBoot init commit

---
+++ src/test/java/com/nkh/ECommerceShop/ECommerceShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nkh.ECommerceShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
