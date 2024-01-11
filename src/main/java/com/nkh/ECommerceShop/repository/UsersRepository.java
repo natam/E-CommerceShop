cae6fe6e7f1685cd671d680f813ecf3136500d2f
natalliakharchenko <morozko.nnn@gmail.com>
2024-01-11 10:43:12 +0100

    Added UserDetailsImpl and UserDetailsServiceImpl classes

--- src/main/java/com/nkh/ECommerceShop/repository/UsersRepository.java
+++ src/main/java/com/nkh/ECommerceShop/repository/UsersRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface _UsersRepository extends JpaRepository<Users,Long> {
+public interface UsersRepository extends JpaRepository<Users,Long> {
     Boolean existsByEmail(String email);
     Optional<Users> findByEmail(String email);
 }
