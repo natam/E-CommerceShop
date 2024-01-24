8f71160ca68fee6bfa62fcdc13a5a2d1bf18ccb8
natalliakharchenko <morozko.nnn@gmail.com>
2024-01-24 10:36:17 +0100

    FP-20 added methods to ProductsService and added endpoint to update product

--- src/main/java/com/nkh/ECommerceShop/service/ProductsService.java
+++ src/main/java/com/nkh/ECommerceShop/service/ProductsService.java
@@ -1,8 +1,10 @@
 package com.nkh.ECommerceShop.service;
 
 import com.nkh.ECommerceShop.exception.AlreadyExistsException;
+import com.nkh.ECommerceShop.exception.ResourceNotFoundException;
 import com.nkh.ECommerceShop.model.Product;
 import com.nkh.ECommerceShop.repository.ProductsRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,5 +21,26 @@ public class ProductsService {
             throw new AlreadyExistsException(String.format("Product with name %s and price %f already exists", product.getName(), product.getPrice()));
         }
         return repository.save(product);
+    }
+
+    public Product getById(long id){
+        return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(String.format("Product with id %d is not found", id)));
+    }
+
+    @Transactional
+    public Product updateProduct(long id, Product product){
+        Product productToUpdate = getById(id);
+        product.setId(productToUpdate.getId());
+        return repository.save(product);
+    }
+
+    public boolean checkProductStock(long productId, int requestedAmount){
+        Product requestedProduct = getById(productId);
+        return requestedProduct.getStock()>=requestedAmount;
+    }
+
+    @Transactional
+    public void deleteProduct(long id){
+        repository.delete(getById(id));
     }
 }
