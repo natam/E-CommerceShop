8ebfd5cce638b1856484ca64e5bfce4054a6db97
natalliakharchenko <morozko.nnn@gmail.com>
2024-01-25 18:14:36 +0100

    FP-57 added fields validation for product

--- src/main/java/com/nkh/ECommerceShop/controller/ProductsController.java
+++ src/main/java/com/nkh/ECommerceShop/controller/ProductsController.java
@@ -2,28 +2,25 @@ package com.nkh.ECommerceShop.controller;
 
 import com.nkh.ECommerceShop.dto.MessageResponseDTO;
 import com.nkh.ECommerceShop.model.Product;
-import com.nkh.ECommerceShop.repository.ProductsRepository;
 import com.nkh.ECommerceShop.service.ProductsService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductsController {
     private final ProductsService productsService;
-    private final ProductsRepository productsRepository;
     @Autowired
-    public ProductsController(ProductsService productsService, ProductsRepository productsRepository){
-        this.productsRepository = productsRepository;
+    public ProductsController(ProductsService productsService){
         this.productsService = productsService;
     }
 
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Product> addProduct(@RequestBody Product product){
+    public ResponseEntity<Product> addProduct(@Valid @RequestBody Product product){
         Product addedProduct = productsService.createProduct(product);
         return ResponseEntity.ok().body(addedProduct);
     }
@@ -31,7 +28,7 @@ public class ProductsController {
     @PutMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Product> updateProduct(@PathVariable("id") long id,
-                                                 @RequestBody Product product){
+                                                 @Valid @RequestBody Product product){
         Product updatedProduct = productsService.updateProduct(id, product);
         return ResponseEntity.ok().body(updatedProduct);
     }
