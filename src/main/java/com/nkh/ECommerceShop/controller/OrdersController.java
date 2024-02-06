9c228f3a4752aadfcb4c17c5e0a7cf75d3c3839d
natalliakharchenko <morozko.nnn@gmail.com>
2024-02-06 12:03:57 +0100

    FP-35 added endpoint to process order from cart

--- src/main/java/com/nkh/ECommerceShop/controller/OrdersController.java
+++ src/main/java/com/nkh/ECommerceShop/controller/OrdersController.java
@@ -1,19 +1,27 @@
 package com.nkh.ECommerceShop.controller;
 
 import com.nkh.ECommerceShop.model.order.Order;
-import com.nkh.ECommerceShop.repository.OrdersRepository;
-import jakarta.validation.OverridesAttribute;
+import com.nkh.ECommerceShop.service.OrdersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/orders")
 public class OrdersController {
+    private final OrdersService ordersService;
 
+    @Autowired
+    public OrdersController(OrdersService ordersService) {
+        this.ordersService = ordersService;
+    }
+
+    @PostMapping("/process/mycart")
+    @PreAuthorize("hasAnyRole('USER')")
+    public ResponseEntity<Order> placeOrderFromMyCart(){
+        return ResponseEntity.ok().body(ordersService.placeOrderFromCart());
+    }
 }
